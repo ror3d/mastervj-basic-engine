591f66291b6290df040ea79b9b19de5cf0a752d5
rcrmn <rcrmn16@gmail.com>
2016-01-26 20:47:24 +0100

    Implemented some functions and captured errors.

--- Base/Scripting/ScriptManager.cpp
+++ Base/Scripting/ScriptManager.cpp
@@ -2,6 +2,30 @@
 
 #include <selene.h>
 
+#include <fstream>
+#include <sstream>
+
+/*
+ * This class captures errors printed by lua to redirect them as we wish.
+ */
+class LuaErrorCapturedStdout {
+public:
+	LuaErrorCapturedStdout() {
+		_old = std::cout.rdbuf(_out.rdbuf());
+	}
+
+	~LuaErrorCapturedStdout() {
+		std::cout.rdbuf(_old);
+	}
+
+	std::string Content() const {
+		return _out.str();
+	}
+
+private:
+	std::stringstream _out;
+	std::streambuf *_old;
+};
 
 CScriptManager::CScriptManager()
 	: m_state(nullptr)
@@ -19,6 +43,7 @@ CScriptManager::~CScriptManager()
 
 void CScriptManager::Initialize()
 {
+	LuaErrorCapturedStdout errorCapture;
 	m_state = new sel::State{ true };
 }
 
@@ -26,19 +51,29 @@ void CScriptManager::Destroy()
 {
 }
 
-void CScriptManager::RunCode(const std::string &Code) const
+void CScriptManager::RunCode(const std::string &code) const
 {
-
+	LuaErrorCapturedStdout errorCapture;
+	(*m_state)(code.c_str());
 }
 
-void CScriptManager::RunFile(const std::string &FileName) const
+void CScriptManager::RunFile(const std::string &fileName) const
 {
+	LuaErrorCapturedStdout errorCapture;
+	std::ifstream f(fileName);
+	std::string code((std::istreambuf_iterator<char>(f)),
+					std::istreambuf_iterator<char>());
+	(*m_state)(code.c_str());
 }
 
-void CScriptManager::Load(const std::string &XMLFile)
+void CScriptManager::Load(const std::string &xmlFile)
 {
+	LuaErrorCapturedStdout errorCapture;
+	// TODO
 }
 
 void CScriptManager::RegisterLUAFunctions()
 {
+	LuaErrorCapturedStdout errorCapture;
+	// TODO
 }
