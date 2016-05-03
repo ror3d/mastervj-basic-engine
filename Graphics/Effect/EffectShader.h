1fda7710dce4c2757832cfed59a7374bceacc327
rcrmn <rcrmn16@gmail.com>
2016-05-03 21:04:28 +0200

    Implemented shader precompilation.

--- Graphics/Effect/EffectShader.h
+++ Graphics/Effect/EffectShader.h
@@ -13,31 +13,33 @@ protected:
 	std::string m_ShaderModel;
 	std::string m_EntryPoint;
 
-	std::vector<ID3D11Buffer *> m_ConstantBuffers;
+	std::vector<ID3D11Buffer*> m_ConstantBuffers;
 	std::vector<std::string> m_PreprocessorMacros;
 	std::vector<D3D10_SHADER_MACRO> m_ShaderMacros;
 
-	bool LoadShader(const std::string &Filename, const std::string &EntryPoint, const std::string &ShaderModel, ID3DBlob **BlobOut);
+	bool LoadShader(const std::string& Filename, const std::string& EntryPoint,
+	                const std::string& ShaderModel, ID3DBlob** BlobOut);
 
 	void CreateShaderMacro();
 	bool CreateConstantBuffer(int IdBuffer, unsigned int BufferSize);
 	bool CreateConstantBuffer();
 public:
-	CEffectShader(const CXMLTreeNode &TreeNode);
+	CEffectShader(const CXMLTreeNode& TreeNode);
 	virtual ~CEffectShader();
-	
+
 	virtual bool Load() = 0;
 	virtual void destroy() = 0;
 
-	virtual void SetConstantBuffer(unsigned int IdBuffer, void *ConstantBuffer) = 0;
+	virtual void SetConstantBuffer(unsigned int IdBuffer, void* ConstantBuffer) = 0;
 
 	virtual bool Reload();
-	inline ID3D11Buffer * GetConstantBuffer(unsigned int IdBuffer) const
+	inline ID3D11Buffer* GetConstantBuffer(unsigned int IdBuffer) const
 	{
 		if (m_ConstantBuffers.size() > IdBuffer)
 		{
 			return m_ConstantBuffers[IdBuffer];
 		}
+
 		return nullptr;
 	}
 };
