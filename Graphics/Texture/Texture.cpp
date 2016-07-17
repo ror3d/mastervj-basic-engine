7e105f6b276d130827b401691d66ea22229f15be
rcrmn <rcrmn16@gmail.com>
2016-07-18 00:18:45 +0200

    Fixed texture mipmapping.

--- Graphics/Texture/Texture.cpp
+++ Graphics/Texture/Texture.cpp
@@ -30,8 +30,26 @@ CTexture::~CTexture()
 bool CTexture::LoadFile(bool linearFilter /*= true*/, bool wrap /*= true*/)
 {
 	ID3D11Device *l_Device = CEngine::GetSingleton().getContextManager()->GetDevice();
+
+	D3DX11_IMAGE_LOAD_INFO loadInfo = {};
+	loadInfo.Width = D3DX11_DEFAULT;
+	loadInfo.Height = D3DX11_DEFAULT;
+	loadInfo.BindFlags = D3DX11_DEFAULT;
+	loadInfo.CpuAccessFlags = D3DX11_DEFAULT;
+	loadInfo.Depth = D3DX11_DEFAULT;
+	loadInfo.Format = DXGI_FORMAT_FROM_FILE;
+	loadInfo.MiscFlags = D3DX11_DEFAULT;
+	loadInfo.pSrcInfo = NULL;
+	loadInfo.Usage = (D3D11_USAGE)D3DX11_DEFAULT;
+	loadInfo.MipFilter = D3DX11_DEFAULT;
+	loadInfo.Filter = D3DX11_DEFAULT;
+	loadInfo.FirstMipLevel = 0;
+	loadInfo.MipLevels = 1;
+
 	HRESULT l_HR = D3DX11CreateShaderResourceViewFromFile(l_Device,
-		getName().c_str(), NULL, NULL, &m_Texture, NULL);
+		getName().c_str(), &loadInfo, NULL, &m_Texture, NULL);
+	D3D11_SHADER_RESOURCE_VIEW_DESC desc;
+	m_Texture->GetDesc( &desc );
 
 	CTextureManager::SamplerStateType type;
 	if ( linearFilter )
