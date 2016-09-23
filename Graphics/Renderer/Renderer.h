2676097b421db52c9a4858482a2e3c24aa90649b
Hector Castro <hector.castrofernandez@gmail.com>
2016-09-23 12:39:41 +0200

    z_sort done.

--- Graphics/Renderer/Renderer.h
+++ Graphics/Renderer/Renderer.h
@@ -11,9 +11,16 @@ class CContextManager;
 class CRenderer
 {
 	typedef std::map<IRenderable*, std::vector<Mat44f>> Layer_t;
+	//typedef std::multimap<std::vector<Mat44f>, IRenderable*> Layer_t_inverse;
 	typedef std::map<std::string, Layer_t> LayerMap_t;
 
 	LayerMap_t m_Layers;
+
+	struct LayerVector{
+		float distance;
+		Matrix44<float> transform;
+		IRenderable* mesh;
+	};
 
 public:
 	CRenderer();
@@ -25,7 +32,13 @@ public:
 
 	void AddRenderableToRender(const std::string& layer, IRenderable* mesh, const Mat44f& transf);
 
-	void RenderLayer(const std::string& layer, CContextManager *context);
+	void RenderLayer(const std::string& layer, CContextManager *context, bool m_layer_zsort);
+
+	void CRenderer::RenderLayerByDepth(LayerMap_t::iterator layerIt, CContextManager *context);
+
+	float CRenderer::CalcDistance(Vect3f dist1, Vect3f dist2);
+
+	static bool CRenderer::CompareDistance(const LayerVector &a, const LayerVector &b);
 
 	void destroy() {}
 };
